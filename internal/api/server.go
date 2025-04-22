3cebb812bd45218034673b3811023b9ab0ea2c4a
ThEditor <theditormcpe@gmail.com>
2025-04-22 23:01:38 +0530

    chore: accept all origins

--- internal/api/server.go
+++ internal/api/server.go
@@ -10,23 +10,6 @@ import (
 	"github.com/ThEditor/clutter-paper/internal/storage"
 )
 
-func corsMiddleware(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		origin := r.Header.Get("Origin")
-		if origin == "http://localhost:5173" || origin == "http://127.0.0.1:5173" {
-			w.Header().Set("Access-Control-Allow-Origin", origin)
-			w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
-			w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
-		}
-		// Handle preflight requests
-		if r.Method == http.MethodOptions {
-			w.WriteHeader(http.StatusOK)
-			return
-		}
-		next.ServeHTTP(w, r)
-	})
-}
-
 func Start(address string, port int, clickhouse *storage.ClickHouseStorage, redis *storage.RedisStorage, postgres *storage.PostgresStorage) {
 	mux := http.NewServeMux()
 
@@ -44,7 +27,7 @@ func Start(address string, port int, clickhouse *storage.ClickHouseStorage, redi
 	})
 
 	log.Info("API server listening on " + address + ":" + strconv.Itoa(port))
-	err := http.ListenAndServe(address+":"+strconv.Itoa(port), corsMiddleware(mux))
+	err := http.ListenAndServe(address+":"+strconv.Itoa(port), mux)
 	if err != nil {
 		log.Info("Server failed to start: " + err.Error())
 	}
