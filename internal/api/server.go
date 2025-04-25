01b1ce0824f311a70970f352f899871a1524dd3a
ThEditor <theditormcpe@gmail.com>
2025-04-25 23:35:40 +0530

    feat: logging

--- internal/api/server.go
+++ internal/api/server.go
@@ -5,28 +5,11 @@ import (
 	"strconv"
 
 	"github.com/ThEditor/clutter-paper/internal/api/common"
+	"github.com/ThEditor/clutter-paper/internal/api/middlewares"
 	"github.com/ThEditor/clutter-paper/internal/api/routes"
 	"github.com/ThEditor/clutter-paper/internal/log"
 	"github.com/ThEditor/clutter-paper/internal/storage"
 )
-
-func corsMiddleware(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		// Allow all origins
-		w.Header().Set("Access-Control-Allow-Origin", "*")
-		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
-		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
-		w.Header().Set("Access-Control-Allow-Credentials", "false")
-
-		// Handle preflight OPTIONS requests
-		if r.Method == "OPTIONS" {
-			w.WriteHeader(http.StatusOK)
-			return
-		}
-
-		next.ServeHTTP(w, r)
-	})
-}
 
 func Start(address string, port int, clickhouse *storage.ClickHouseStorage, redis *storage.RedisStorage, postgres *storage.PostgresStorage) {
 	mux := http.NewServeMux()
@@ -45,7 +28,7 @@ func Start(address string, port int, clickhouse *storage.ClickHouseStorage, redi
 	})
 
 	log.Info("API server listening on " + address + ":" + strconv.Itoa(port))
-	err := http.ListenAndServe(address+":"+strconv.Itoa(port), corsMiddleware(mux))
+	err := http.ListenAndServe(address+":"+strconv.Itoa(port), middlewares.Cors(middlewares.Logger(mux)))
 	if err != nil {
 		log.Info("Server failed to start: " + err.Error())
 	}
